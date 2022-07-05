d952836dc381ca7660997b70619c76a35a736f0d
Demi Hayashi <demi.h@me.com>
2022-07-05 14:28:01 -0500

    add foundation for javascript

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+function card(name, description, image) {
+    this.name = name;
+    this.description = description;
+    this.image = image;
+};
+
+var deck = [
+    new card('Card One', '(description)', 'image.jpg'),
+    new card('Card One', '(description)', 'image.jpg'),
+    new card('Card One', '(description)', 'image.jpg'),
+    new card('Card One', '(description)', 'image.jpg'),
+    new card('Card One', '(description)', 'image.jpg'),
+    new card('Card One', '(description)', 'image.jpg'),
+    new card('Card One', '(description)', 'image.jpg'),
+    new card('Card One', '(description)', 'image.jpg')
+]
